6ce07e5ed2d5723b51ba77bea44042d214f98955
ElifRuveyha <ruveyhaozmen@gmail.com>
2024-05-15 16:02:02 +0200

    decrease priorities of drops and update queue id

--- scripts/create_virtual_queues_drop_connections.sh
+++ scripts/create_virtual_queues_drop_connections.sh
@@ -36,7 +36,7 @@ queues:3=@devq -- \
 sudo ovs-vsctl set port s2-eth4 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=1000000000 \
-queues:2=@mgmtq \
+queues:4=@mgmtq \
 queues:3=@devq -- \
 --id=@mgmtq create queue other-config:min-rate=10000 other-config:max-rate=400000000 -- \
 --id=@devq create queue other-config:min-rate=10000 other-config:max-rate=200000000
@@ -45,15 +45,16 @@ sudo ovs-vsctl set port s2-eth5 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=1000000000 \
 queues:1=@prodq \
-queues:2=@mgmtq -- \
+queues:4=@mgmtq -- \
 --id=@prodq create queue other-config:min-rate=10000 other-config:max-rate=500000000 -- \
 --id=@mgmtq create queue other-config:min-rate=10000 other-config:max-rate=400000000
+
 
 #creating virtual queues for s3
 sudo ovs-vsctl set port s3-eth3 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=1000000000 \
-queues:2=@mgmtq \
+queues:4=@mgmtq \
 queues:3=@devq -- \
 --id=@mgmtq create queue other-config:min-rate=10000 other-config:max-rate=400000000 -- \
 --id=@devq create queue other-config:min-rate=10000 other-config:max-rate=200000000
@@ -69,9 +70,10 @@ sudo ovs-vsctl set port s4-eth3 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
 other-config:max-rate=1000000000 \
 queues:1=@prodq \
-queues:2=@mgmtq -- \
+queues:4=@mgmtq -- \
 --id=@prodq create queue other-config:min-rate=10000 other-config:max-rate=500000000 -- \
 --id=@mgmtq create queue other-config:min-rate=10000 other-config:max-rate=400000000
+
 
 sudo ovs-vsctl set port s4-eth4 qos=@newqos -- \
 --id=@newqos create QoS type=linux-htb \
@@ -84,66 +86,66 @@ echo '----------- Dropping all connections ------------'
 
 #dropping the connection of all hosts
 
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.1,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.1,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.1,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.1,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.1,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.1,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.1,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
 
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.2,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.2,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.2,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.2,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.2,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.2,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.2,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s1 ip,priority=500,nw_src=10.0.0.2,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
 
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.3,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.3,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.3,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.3,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.3,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.3,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.3,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.3,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
 
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.4,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.4,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.4,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.4,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.4,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.4,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.4,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s2 ip,priority=500,nw_src=10.0.0.4,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
 
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.5,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.5,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.5,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.5,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.5,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.5,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.5,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.5,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.5,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.5,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.5,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.5,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.5,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.5,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
 
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.6,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.6,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.6,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.6,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.6,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.6,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s3 ip,priority=500,nw_src=10.0.0.6,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
 
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.7,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.7,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.7,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.7,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.7,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.7,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.7,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.7,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.7,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.7,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.7,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.7,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.7,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.7,nw_dst=10.0.0.8,idle_timeout=0,actions=drop
 
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.8,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.8,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.8,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.8,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.8,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.8,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
-sudo ovs-ofctl add-flow s4 ip,priority=65500,nw_src=10.0.0.8,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.8,nw_dst=10.0.0.1,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.8,nw_dst=10.0.0.2,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.8,nw_dst=10.0.0.3,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.8,nw_dst=10.0.0.4,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.8,nw_dst=10.0.0.5,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.8,nw_dst=10.0.0.6,idle_timeout=0,actions=drop
+sudo ovs-ofctl add-flow s4 ip,priority=500,nw_src=10.0.0.8,nw_dst=10.0.0.7,idle_timeout=0,actions=drop
