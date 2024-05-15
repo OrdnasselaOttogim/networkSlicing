3a28369829dfd68fb6e5123baae45843e107a33c
ElifRuveyha <ruveyhaozmen@gmail.com>
2024-05-15 16:02:34 +0200

    fix development slice

--- scripts/development_slice.sh
+++ scripts/development_slice.sh
@@ -8,6 +8,11 @@ echo '----------------------h1 h6-----------------------'
 
 sudo ovs-ofctl add-flow s1 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.6,idle_timeout=0,actions=set_queue:3,normal
 
+#s2
+
+sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.1,nw_dst=10.0.0.6,idle_timeout=0,actions=normal
+sudo ovs-ofctl add-flow s2 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.1,idle_timeout=0,actions=normal
+
 #s3 h6
 
 sudo ovs-ofctl add-flow s3 ip,priority=65500,nw_src=10.0.0.6,nw_dst=10.0.0.1,idle_timeout=0,actions=set_queue:3,output:3
